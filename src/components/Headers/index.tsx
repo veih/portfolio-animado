29743e7fe8c4ba8402fea8c2a386d2ef555f20d3
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-07-19 15:49:45 -0300

    reajustes

--- src/components/Headers/index.tsx
+++ src/components/Headers/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { React} from 'react'; 
 
 import './Header.css';
 
@@ -21,40 +21,49 @@ export const Header = () => {
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <Link className="nav-link" to="/portfolio-animado/">Home</Link>
-                            <Link className="nav-link" to="/pages/Cocacola">Coca Cola</Link>
+                            <div className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Projetos
+                                </Link>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <div><Link className="dropdown-item" to="/pages/Cocacola">Coca Cola</Link></div>
+                                    <div><Link className="dropdown-item" to="#">Another action</Link></div>
+                                    <div><Link className="dropdown-item" to="#">Something else here</Link></div>
+                                </div>
+                            </div>
                             <Link className="nav-link" to="/pages/Cursos">Cursos</Link>
-                            <Link className="nav-link" to="#">Pricing</Link>
+                            <Link className="nav-link" to="/pages/Cursos">Pricing</Link>
 
                         </div>
                     </div>
                 </div>
                 <div className=" icons">
-                    <a href="https://github.com/veih" target="_blank" rel="noopener noreferrer">
+                    <Link to="https://github.com/veih" target="_blank" rel="noopener noreferrer">
 
                         <i className="bi bi-github" id="github"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/marcelo-evangelista-847515175/" target="_blank"
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/marcelo-evangelista-847515175/" target="_blank"
                         rel="noopener noreferrer">
                         <i className="bi bi-linkedin" id="linkedin"></i>
 
-                    </a>
-                    <a href="https://www.instagram.com/marceloveih/" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link to="https://www.instagram.com/marceloveih/" target="_blank" rel="noopener noreferrer">
 
                         <i className="bi bi-instagram" id="instagram"></i>
-                    </a>
-                    <a href="https://www.facebook.com/veihbabal/" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link to="https://www.facebook.com/veihbabal/" target="_blank" rel="noopener noreferrer">
 
                         <i className="bi bi-facebook" id="facebook"></i>
-                    </a>
-                    <a href="#(85)999568243" onClick={() => { alert("Para contato, entre em contato pelo telefone: (85)9995-68243") }}
+                    </Link>
+                    <Link to="#(85)999568243" onClick={() => { alert("Para contato, entre em contato pelo telefone: (85)9995-68243") }}
                         rel="noopener noreferrer">
 
                         <i className="bi bi-whatsapp" id="whatsapp"></i>
-                    </a>
-                    <a href="https://twitter.com/Babal92258376" target="_blank" rel="noopener noreferrer">
+                    </Link>
+                    <Link to="https://twitter.com/Babal92258376" target="_blank" rel="noopener noreferrer">
 
                         <i className="bi bi-twitter" id="twitter"></i>
-                    </a>
+                    </Link>
                 </div>
             </nav>
         </header>
