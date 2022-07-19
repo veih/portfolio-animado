0e79c413e253bf848599ba92889a30ef64a6fc17
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-07-18 21:15:04 -0300

    reajustes

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -4,11 +4,10 @@ import { Main } from "./components/Mains"
 
 import { CocaCola } from "./pages/Cocacola"
 import { Cursos } from "./pages/Cursos"
-import { Home } from "./pages/Home"
 
 export const Routers = () => {
     return useRoutes([
-        { path: "/portfolio-animado/", element: <Home /> },
+        { path: "/portfolio-animado/", element: <Main /> },
         { path: "/home", element: <Main /> },
         { path: "/pages/Cocacola", element: <CocaCola /> },
         { path: "/pages/Cursos", element: <Cursos /> },
--- src/pages/Cursos/index.jsx
+++ src/pages/Cursos/index.jsx
@@ -1,10 +1,16 @@
-import { Cards } from "../../components/Cards"
+import React from "react"
 
 export const Cursos = () => {
 
     return (
         <div>
-            em produção
+            <div className="Container ">
+                <div className="">
+                    <h4 className="text-primary ">Espero que esteja procurando um Dev Front-End!</h4>
+                    <p className="text-primary ">Senta-se a vontade para olhar os meus projetos</p>
+                </div>
+
+            </div>
         </div>
     )
 
