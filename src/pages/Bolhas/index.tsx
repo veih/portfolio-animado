65bddbe726ef51d7c06a65ef02b3e41c35d99234
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-07-26 22:49:52 -0300

    ajustes

--- src/pages/Bolhas/index.tsx
+++ src/pages/Bolhas/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Span } from "../../components/spans";
 export const Bolhas = () => {
     return (
-        <div>
+        <div className="sabao">
             <div className="bolhas">
                 <Span />
                 <Span />
