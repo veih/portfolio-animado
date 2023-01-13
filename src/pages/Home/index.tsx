f62f05ef5ab4ccfa2c3c540f05d700721c0b19fd
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2023-01-13 11:44:17 -0300

    refaturação do cod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-import { Section } from "../../components/Section";
-
 import "./home.css";
-
 
 export const Home = () => {
   return (
     <main className="home">
-      <Section />
+      
+      <div className="book">
+        
+        <div className="frontCoverFace"></div>
+        <div className="leaf"></div>
+        <div className="leaf"></div>
+        <div className="leaf"></div>
+        <div className="leaf"></div>
+        <div className="leaf"></div>
+
+        <div className="frontCoverOut">
+          <h2 className="text-pop-up-top ">
+            Diário
+            <br /> do <br /> Marcelo
+          </h2>
+        </div>
+        
+      </div>
+      <h5>Em desenvolvimento em breve estara finalizado.</h5>
     </main>
   );
 };
