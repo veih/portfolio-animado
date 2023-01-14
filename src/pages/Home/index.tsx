bb2eba090c25b9eb787733f2f79a4ca13630041f
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2023-01-14 10:52:51 -0300

    adicionando os efeitos da capa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 import "./home.css";
 
 export const Home = () => {
+  const [id, setId] = useState("frontCoverOut");
+
+  const handleFlip = () => {
+    
+    if (id === "frontCoverOutFlip") {
+      setId("frontCoverOut");
+    } else {
+      setId("frontCoverOutFlip");
+    }
+  };
+
+  const handleFlipOut = () => {};
+
   return (
     <main className="home">
-      
       <div className="book">
-        
         <div className="frontCoverFace"></div>
         <div className="leaf"></div>
         <div className="leaf"></div>
         <div className="leaf"></div>
         <div className="leaf"></div>
-        <div className="leaf"></div>
-
-        <div className="frontCoverOut">
-          <h2 className="text-pop-up-top ">
-            Diário
-            <br /> do <br /> Marcelo
-          </h2>
-        </div>
-        
+        <div className="leaf">5</div>
+        <div className={id} onClick={handleFlip}></div>
       </div>
+
       <h5>Em desenvolvimento em breve estara finalizado.</h5>
     </main>
   );
